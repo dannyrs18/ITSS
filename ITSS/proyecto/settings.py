5d0d680f602a4919a58b26bcc7bd22f2b6e5f735
Danny Romero <dannyors18@gmail.com>
2019-01-26 12:47:11 -0500

    modificado servidor

--- ITSS/proyecto/settings.py
+++ ITSS/proyecto/settings.py
@@ -91,10 +91,6 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
         },
     }, # LA BASE DE DATOS TIENE PRIVILEGIOS LIMITADOS
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
