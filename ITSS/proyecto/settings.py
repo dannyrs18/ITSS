39b2c109bab09cb7f42bd235a9d7b953b15c2ef8
Danny Romero <dannyors18@gmail.com>
2019-01-26 12:10:42 -0500

    dos base de datos

--- ITSS/proyecto/settings.py
+++ ITSS/proyecto/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'practicas',
         'USER': 'dep_practicas',
@@ -92,6 +90,10 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
         },
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
     } # LA BASE DE DATOS TIENE PRIVILEGIOS LIMITADOS
 }
 
